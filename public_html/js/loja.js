316ae3e63a94d7eaa7f83878df822b9dbd87a4e6
jonata.hmoliveira <jonata.hmoliveira@CAS555511W0713.senacsp.edu.br>
2017-09-21 20:44:59 -0300

    Baguio doido!

--- public_html/js/loja.js
+++ public_html/js/loja.js
@@ -1,6 +1,5 @@
 // cria a aplicação e indica que vamos usar o plugin de rotas
 var app = angular.module('lojaApp', ['ngRoute']);
-
 // configura o plugin de rotas
 app.config(function ($routeProvider) {
     $routeProvider
@@ -64,3 +63,15 @@ app.controller('lojaCtrl', function ($scope, $http, $routeParams) {
         return value.preco >= $scope.min && value.preco <= $scope.max;
     };
 });
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register('LOJA-SW.js')
+                .then(function (registration) {
+                    console.log('Service Worker OK!');
+                }, function (err) {
+                    console.log('Service Worker NOT OK', err);
+                });
+    });
+}
